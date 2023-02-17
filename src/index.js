3d3120787958d4ee4a5ecaf95dc36aa04d9512f6
Olha Savva <106154412+OlhaSavva@users.noreply.github.com>
2023-02-18 01:09:19 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
-
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('input'),
+};
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {
+  event.preventDefault();
+  const inputValue = event.target.value.trim();
+
+  fetchCountries(inputValue).then(console.log);
+}
