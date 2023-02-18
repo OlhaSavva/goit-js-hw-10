6dc1b958e717514cf32e6c004e92b7de99ccc2ef
OlhaSavva <olasavva@gmail.com>
2023-02-18 18:33:40 +0200

    finish HW 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,11 @@
 function fetchCountries(name) {
   const URL = `https://restcountries.com/v3.1/name/${name}`;
-  return fetch(URL).then(response => response.json());
+  return fetch(URL).then(response=>{
+    if(response.ok){
+      return response.json();
+    }
+    throw new Error("Oops, there is no country with this name");
+  });
 }
 
 export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
   input: document.querySelector('input'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
@@ -13,5 +17,53 @@ function onInput(event) {
   event.preventDefault();
   const inputValue = event.target.value.trim();
 
-  fetchCountries(inputValue).then(console.log);
+  fetchCountries(inputValue)
+    .then(countries => {
+      refs.countryList.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
+
+      if (countries.length === 1) {
+        console.log(countries);
+
+        const markup = createMarkupForCountryInfo(countries[0]);
+        refs.countryInfo.innerHTML = markup;
+      } else if (countries.length > 1 && countries.length <= 10) {
+        console.log(countries);
+        const markup = createMarkupForCountryList(countries);
+        refs.countryList.innerHTML = markup;
+      } else {
+        Notify.info(
+          'Too many matches found. Please enter a more specific query!'
+        );
+      }
+    })
+    .catch((error) => {
+      refs.countryList.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
+
+      Notify.failure(error.message)});
+}
+
+function createMarkupForCountryInfo(country) {
+  const languages = Object.values(country.languages);
+  const languagesStr = languages.length > 1 ? languages.join(', ') : languages[0];
+  return `
+    <li class="country">
+      <p class="country__name">${country.flag} ${country.name.common}</p>
+      <p class="country__capital"><strong>Capital: </strong>${country.capital}</p>
+      <p class="country__population"><strong>Population: </strong>${country.population}</p>
+      <p class="country__languages"><strong>Languages: </strong>${languagesStr}</p>
+    </li>
+  `;
+}
+
+
+function createMarkupForCountryList(countries) {
+  return countries.map(country => countryListMarkup(country)).join('');
+}
+
+function countryListMarkup(country) {
+  return `
+    <p class="country__name-list">${country.flag} ${country.name.common}</p>
+  `;
 }
